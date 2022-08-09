8a1a93ae62a43bb9694c0feafc22407716783010
Rama <86864383+ramamdeeCode@users.noreply.github.com>
2022-08-09 19:33:49 -0400

    create handler and assosiate middleware

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -8,11 +8,54 @@ const nextId = require("../utils/nextId");
 
 // TODO: Implement the /dishes handlers needed to make the tests pass
 
+///validate price property if is greater than an int and greater than 0
+const pricePropertyIsvalid = (req, res, next) => {
+  const { data: { price } = {} } = req.body;
+  return Number.isInteger(Number(price)) && price > 0
+    ? next()
+    : next({
+        status: 400,
+        message: "Dish must have a price that is an integer greater than 0",
+      });
+};
+
+const bodyDataHas = (propertyName) => {
+  return (req, res, next) => {
+    const { data = {} } = req.body;
+    if (data[propertyName]) return next();
+    next({ status: 400, message: `Must include a ${propertyName}` });
+  };
+};
+
+//create dish
+const create = (req, res, next) => {
+  const { data: { name, description, price, image_url } = {} } = req.body;
+
+  const newDish = {
+    id: nextId(),
+    name,
+    description,
+    price,
+    image_url,
+  };
+
+  dishes.push(newDish);
+  res.status(201).json({ data: newDish });
+};
+
 //get all the dishes
 const list = (req, res) => {
   res.json({ data: dishes });
 };
 
 module.exports = {
+  create: [
+    bodyDataHas("name"),
+    bodyDataHas("description"),
+    bodyDataHas("price"),
+    bodyDataHas("image_url"),
+    pricePropertyIsvalid,
+    create,
+  ],
   list,
 };
